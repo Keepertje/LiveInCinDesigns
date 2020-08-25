305559dc699bdff1b796dc00de1e1a03884d04cc
Cindy Berghuizen <cindy.berghuizen@ns.nl>
2020-08-25 19:39:41 +0200

    extra page, online logo's

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(window).scroll(function(){
+  if ($(window).scrollTop() >= 100) {
+      $('.navbar').addClass('fixed-header');
+  }
+  else {
+      $('.navbar').removeClass('fixed-header');
+  }
+});
+
+(function($){
+  $(window).on('load', function(){
+      $.instagramFeed({
+          'username': 'liveincin_dy',
+          'container': ".insta-feed",
+          'display_profile': false,
+          'display_biography': false,
+          'display_gallery': true,
+          'callback': null,
+          'styling': true,
+          'items': 4,
+          'items_per_row': 4,
+          'margin': 1,
+          'lazy_load': true,
+          'on_error': console.error
+      });
+  });
+})(jQuery);
